467c1b923b7c6c25bae4d1f52cd18c734271f542
davakanbendicto@gmail.com <davakanbendicto@gmail.com>
2022-05-09 09:03:27 +0100

    création d'une page

--- src/TdsBooking/settings.py
+++ src/TdsBooking/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TdsBooking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
